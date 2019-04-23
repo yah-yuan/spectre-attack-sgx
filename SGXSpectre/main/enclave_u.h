e53c8bf5502e4d7dab8ed492e1e4481920e9d637
yuan <0505yahweh@gmail.com>
2019-04-24 02:03:56 +0800

    .

---
+++ SGXSpectre/main/enclave_u.h
@@ -0,0 +1,32 @@
+#ifndef ENCLAVE_U_H__
+#define ENCLAVE_U_H__
+
+#include <stdint.h>
+#include <wchar.h>
+#include <stddef.h>
+#include <string.h>
+#include "sgx_edger8r.h" /* for sgx_satus_t etc. */
+
+
+#include <stdlib.h> /* for size_t */
+
+#define SGX_CAST(type, item) ((type)(item))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void SGX_UBRIDGE(SGX_CDECL, sgx_oc_cpuidex, (int cpuinfo[4], int leaf, int subleaf));
+int SGX_UBRIDGE(SGX_CDECL, sgx_thread_wait_untrusted_event_ocall, (const void* self));
+int SGX_UBRIDGE(SGX_CDECL, sgx_thread_set_untrusted_event_ocall, (const void* waiter));
+int SGX_UBRIDGE(SGX_CDECL, sgx_thread_setwait_untrusted_events_ocall, (const void* waiter, const void* self));
+int SGX_UBRIDGE(SGX_CDECL, sgx_thread_set_multiple_untrusted_events_ocall, (const void** waiters, size_t total));
+
+sgx_status_t ecall_get_offset(sgx_enclave_id_t eid, size_t* retval);
+sgx_status_t ecall_victim_function(sgx_enclave_id_t eid, size_t x, uint8_t* array2, unsigned int* outside_array1_size);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
--- SGXSpectre/main/main.c
+++ SGXSpectre/main/main.c
@@ -37,9 +37,9 @@ unsigned int array1_size = 16;
 struct timespec measure_time1, measure_time2;
 FILE * log_file;
 char * file_name = "measure.log";
-char *secret = "The Magic Words are Squeamish Ossifrage.";
+char *secret = "At a seminar in the Bell Communications Research Colloquia Series, Dr. Richard W. Hamming, a Professor at the Naval Postgraduate School in Monterey, California and a retired Bell Labs scientist, gave a very interesting and stimulating talk, `You and Your Research' to an overflow audience of some 200 Bellcore staff members and visitors at the Morris Research and Engineering Center on March 7, 1986. This talk centered on Hamming's observations and research on the question ``Why do so few scientists make significant contributions and so many are forgotten in the long run?'' From his more than forty years of experience, thirty of which were at Bell Laboratories, he has made a number of direct observations, asked very pointed questions of scientists about what, how, and why they did things, studied the lives of great scientists and great contributions, and has done introspection and studied theories of creativity. The talk is about what he has learned in terms of the properties of the individual scientists, their abilities, traits, working habits, attitudes, and philosophy. ";
 int correct = 0;
-long long int the_smallest = 9999999;
+long long int the_smallest = 99999999999;
 
 uint8_t unused1[64];
 uint8_t array1dupe[160] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
@@ -53,7 +53,7 @@ uint8_t array2[256 * 512];
  #define TRY_TIMES (1)
  #define TRAINING_ROUNDS (1)
  #define TRAINS_PER_ROUND (1) 
- #define SECRET_LEN (40)
+ #define SECRET_LEN (1086)
 
  /* Report best guess in value[0] and runner-up in value[1] */
  void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
@@ -158,13 +158,13 @@ int spectre_main(int argc, char **argv) {
 	printf("Reading %d bytes:\n", len);
 	
 	while (--len >= 0) {
-		printf("Reading at malicious_x = %p... ", (void*)malicious_x);
+		// printf("Reading at malicious_x = %p... ", (void*)malicious_x);
 		readMemoryByte(malicious_x++, value, score);
-		printf("%s: ", (score[0] >= 2*score[1] ? "Success" : "Unclear"));
-		printf("0x%02X='%c' score=%d ", value[0], (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
-		if (score[1] > 0)
-			printf("(second best: 0x%02X score=%d)", value[1], score[1]);
-		printf("\n");
+		// printf("%s: ", (score[0] >= 2*score[1] ? "Success" : "Unclear"));
+		// printf("0x%02X='%c' score=%d ", value[0], (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
+		// if (score[1] > 0)
+		// 	printf("(second best: 0x%02X score=%d)", value[1], score[1]);
+		// printf("\n");
 		if (value[0] == *(secret+(SECRET_LEN-len))) {
 			correct += 1;
 		}
@@ -181,6 +181,12 @@ int spectre_main(int argc, char **argv) {
 int main(int argc, char *argv[])
 {
 	log_file = fopen(file_name, "wb");
+	if (log_file == 0)
+	{
+		printf("openfailed\n");
+		exit(1);
+	}
+	
     /* Initialize the enclave */
     initialize_enclave();
  
